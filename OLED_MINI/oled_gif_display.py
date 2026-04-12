0288e81d99c47ae603cfef83e14d84d922fe2b0b
Dane404 <danijelpejic15@gmail.com>
2026-04-12 20:50:00 +0200

    sound play

--- OLED_MINI/oled_gif_display.py
+++ OLED_MINI/oled_gif_display.py
@@ -1,16 +1,16 @@
 from luma.core.interface.serial import i2c
 from luma.oled.device import sh1106  # if this fails, swap to ssd1306
 from PIL import Image, ImageSequence
+from playsound import playsound
 import time
 # connect to OLED
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial)
 
 # create FULL white image
-
+playsound('../Sounds/monotheater.mp3')
 background = Image.new("RGBA", (device.width, device.height))
-gif = Image.open("/home/dane/Downloads/monokuma-danganronpa.gif")
-gi2f = gif.resize((device.width, device.height))
+gif = Image.open("../Pictures/monokuma-danganronpa.gif")
 posn = (25,0)
 while True:
    for frame in ImageSequence.Iterator(gif):
