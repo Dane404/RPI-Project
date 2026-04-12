761b91193520e2a4ddb2246dda50a400a436ffa7
Dane404 <danijelpejic15@gmail.com>
2026-04-12 21:01:06 +0200

    bug fixes and finishing touch oled player

--- OLED_MINI/oled_gif_display.py
+++ OLED_MINI/oled_gif_display.py
@@ -1,20 +1,22 @@
 from luma.core.interface.serial import i2c
 from luma.oled.device import sh1106  # if this fails, swap to ssd1306
 from PIL import Image, ImageSequence
-from playsound import playsound
+from playsound3 import playsound
+import os
+
 import time
 # connect to OLED
 serial = i2c(port=1, address=0x3C)
 device = sh1106(serial)
 
 # create FULL white image
-playsound('../Sounds/monotheater.mp3')
+playsound(os.getcwd() +'/Sounds/monotheater.mp3',False)
 background = Image.new("RGBA", (device.width, device.height))
-gif = Image.open("../Pictures/monokuma-danganronpa.gif")
+gif = Image.open(os.getcwd() +"/Pictures/monokuma-danganronpa.gif")
 posn = (25,0)
 while True:
    for frame in ImageSequence.Iterator(gif):
     background = Image.new("RGBA", (device.width,device.height))
     background.paste(frame.resize((device.width-50,device.height),resample=Image.HAMMING),posn)
     device.display(background.convert(device.mode))
-    time.sleep(0.5)
+    time.sleep(0.05)
