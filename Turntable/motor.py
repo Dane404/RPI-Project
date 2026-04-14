3a0507b1e728e50b409d9c0f1cceaed896045549
Dane404 <danijelpejic15@gmail.com>
2026-04-14 19:28:52 +0200

    bla

--- Turntable/motor.py
+++ Turntable/motor.py
@@ -2,7 +2,7 @@ from gpiozero import OutputDevice
 import time
 motorPins = (12,16,20,21)
 
-motors = list(map(lambda pin: OutputDevice(pin),motorPins))
-while True:
-    for i in range(0,4,1):
-        motors[i].on
+motor= OutputDevice(12)
+motors.on
+
+        
