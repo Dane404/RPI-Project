98fa86dcb3c63f5d703368e3eaa0ee0b3fe17d5d
Dane404 <danijelpejic15@gmail.com>
2026-04-14 19:40:12 +0200

    bla

--- Turntable/motor.py
+++ Turntable/motor.py
@@ -3,11 +3,11 @@ import time
 motorPins = (12,16,20,21)
 motors = list(map(lambda pin: OutputDevice(pin), motorPins))
 while True:
-    for i in (0,4,1):
+    for i in (0,3,1):
         if i>=1:
             motors[i-1].off()
             motors[i].on()
         else:
             motors[3].off()
             motors[i].on()
-        time.sleep(1)
+        time.sleep(2)
