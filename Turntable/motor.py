b233dd34a9413145f4739c43d2e72cbfcebc6371
Dane404 <danijelpejic15@gmail.com>
2026-04-16 22:01:06 +0200

    bla

--- Turntable/motor.py
+++ Turntable/motor.py
@@ -1,13 +1,35 @@
 from gpiozero import OutputDevice
-import time
-motorPins = (12,16,20,21)
-motors = list(map(lambda pin: OutputDevice(pin), motorPins))
-while True:
-    for i in (0,3,1):
-        if i>=1:
-            motors[i-1].off()
-            motors[i].on()
-        else:
-            motors[3].off()
-            motors[i].on()
-        time.sleep(2)
+from time import sleep
+
+IN1 = OutputDevice(12)
+IN2 = OutputDevice(16)
+IN3 = OutputDevice(20)
+IN4 = OutputDevice(21)
+
+pins = [IN1, IN2, IN3, IN4]
+
+# Base sequence
+seq = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+]
+
+def stepmotor(steps, direction="CW", delay=0.003):
+    sequence = seq if direction == "CW" else list(reversed(seq))
+
+    for _ in range(steps):
+        for step in sequence:
+            for pin, state in zip(pins, step):
+                if state:
+                    pin.on()
+                else:
+                    pin.off()
+            sleep(delay)
+
+try:
+    stepmotor(512, direction="CW")   # change to "CCW" if needed
+finally:
+    for pin in pins:
+        pin.off()
