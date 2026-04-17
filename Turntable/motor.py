6ba564fdd4dc8feff676ff6643c54c7f1839fe7c
Dane404 <danijelpejic15@gmail.com>
2026-04-17 11:24:13 +0200

    bla

--- Turntable/motor.py
+++ Turntable/motor.py
@@ -1,35 +1,26 @@
-from gpiozero import OutputDevice
-from time import sleep
-
-IN1 = OutputDevice(12)
-IN2 = OutputDevice(16)
-IN3 = OutputDevice(20)
-IN4 = OutputDevice(21)
-
-pins = [IN1, IN2, IN3, IN4]
-
-# Base sequence
-seq = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-]
-
-def stepmotor(steps, direction="CW", delay=0.003):
-    sequence = seq if direction == "CW" else list(reversed(seq))
-
-    for _ in range(steps):
-        for step in sequence:
-            for pin, state in zip(pins, step):
-                if state:
-                    pin.on()
-                else:
-                    pin.off()
-            sleep(delay)
-
-try:
-    stepmotor(512, direction="CW")   # change to "CCW" if needed
-finally:
-    for pin in pins:
-        pin.off()
+from gpiozero import DigitalOutputDevice, Button
+import time
+relayPin = 17 # define the relayPin
+buttonPin = 18 # define the buttonPin
+relay = DigitalOutputDevice(relayPin) # define LED pin according to BCM Numbering
+button = Button(buttonPin) # define Button pin according to BCM Numbering
+def onButtonPressed(): # When button is pressed, this function will be executed
+    relay.toggle()
+    if relay.value :
+        print("Turn on relay ...")
+    else :
+        print("Turn off relay ... ")
+def loop():
+    button.when_pressed = onButtonPressed
+    while True:
+        time.sleep(1)
+def destroy():
+    relay.close()
+    button.close()
+    if __name__ == '__main__': # Program entrance
+        print ('Program is starting...')
+    try:
+        loop()
+    except KeyboardInterrupt: # Press ctrl-c to end the program.
+        destroy()
+        print("Ending program")
